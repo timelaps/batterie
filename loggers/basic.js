1864d195235688ee73eadb3c363998d810bf0c1d
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-04-14 18:43:48 -0500

    updated basic logger to output missed and failed items

--- loggers/basic.js
+++ loggers/basic.js
@@ -5,26 +5,16 @@ function basicLog(batterie) {
     return function () {
         var expectations = batterie.expectations;
         var its = batterie.its;
-        forEach(expectations.missed, function (missed) {
-            console.log(missed.pretty());
+        var missed = [];
+        var failed = [];
+        forEach(expectations.missed, function (item) {
+            missed.push(item.pretty());
         });
-        forEach(expectations.failed, function (failed) {
-            console.log(failed.pretty());
+        forEach(expectations.failed, function (item) {
+            failed.push(item.pretty());
         });
-        batterie.write('results', ['RESULTS', //
+        batterie.write('results', missed.concat(failed, ['RESULTS', //
             'TOTAL:\t' + its.every.length, 'PASSED:\t' + its.passed.length, 'FAILED:\t' + its.failed.length, 'MISSED:\t' + its.missed.length, 'SYNC:\t' + its.sync.length, 'ASYNC:\t' + its.async.parallel.length, 'SERIAL:\t' + its.async.serial.length, 'EXPECTATIONS', 'TOTAL:\t' + expectations.every.length, 'PASSED:\t' + expectations.passed.length, 'FAILED:\t' + expectations.failed.length
-        ]);
-        // console.log('RESULTS\nTOTAL:\t' + its.every.length + //
-        //     '\nPASSED:\t' + its.passed.length + //
-        //     '\nFAILED:\t' + its.failed.length + //
-        //     '\nMISSED:\t' + its.missed.length + //
-        //     '\nSYNC:\t' + its.sync.length + //
-        //     '\nASYNC:\t' + its.async.parallel.length + //
-        //     '\nSERIAL:\t' + its.async.serial.length + //
-        //     '\n\nEXPECTATIONS' + //
-        //     '\nTOTAL:\t' + expectations.every.length + //
-        //     '\nPASSED:\t' + expectations.passed.length + //
-        //     '\nFAILED:\t' + expectations.failed.length //
-        // );
+        ]))
     };
 }
