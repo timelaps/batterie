c1012954789dacb35747ff6f9e3af8a445e0c694
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-13 20:40:49 -0500

    added package lock and added coveralls to packages

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,39 @@
 #!/usr/bin/env node
 
+var path = require('path');
 var argv = process.argv;
 var command = argv[2];
 var cwd = process.cwd();
 var commands = {
     lash: function (from, to) {
-        var path = require('path');
         return require('./lash')(from[0] === '/' ? from : path.join(cwd, from), to[0] === '/' ? to : path.join(cwd, to));
     },
-    findEmpty: function (inside) {
-        var path = require('path');
+    'find-empty': function (inside) {
         return require('./find-empty')(inside[0] === '/' ? inside : path.join(cwd, inside));
     },
     '--version': printVersion,
-    '-v': printVersion
+    '-v': printVersion,
+    test: function (totest) {
+        var relativeScriptPath = totest || mainScriptPath();
+        var p = path.join(cwd, relativeScriptPath);
+        require(p);
+    }
 };
 var cmd = commands[command];
 if (!cmd) {
-    return console.log('unknown command');
+    if (!argv[3]) {
+        cmd = commands.test;
+    } else {
+        return console.log('unknown command');
+    }
 }
 cmd.apply(null, argv.slice(3));
+
+function mainScriptPath() {
+    var targetjson = require(path.join(cwd, 'package.json'));
+    var batt = targetjson.test;
+    return batt && batt.root || 'test.js';
+}
 
 function printVersion() {
     var package = require('../package.json');
